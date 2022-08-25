339c72fdebd05af19f091287d6684df09c90b276
africalib <africalibrary21@gmail.com>
2022-08-26 08:38:05 +0900

    database

---
+++ vue3/gallery/database/gallery_items.sql
@@ -0,0 +1,3 @@
+INSERT INTO gallery.items (id, name, img_path, price, discount_per) VALUES (1, 'vintage', '/img/vintage-g537450080_640.jpg', 10000000, 5);
+INSERT INTO gallery.items (id, name, img_path, price, discount_per) VALUES (2, 'post', '/img/post-impressionist-g3204e3043_640.jpg', 20000000, 10);
+INSERT INTO gallery.items (id, name, img_path, price, discount_per) VALUES (3, 'vincent', '/img/vincent-van-gogh-ge8c415979_640.jpg', 30000000, 30);
---
+++ vue3/gallery/database/gallery_members.sql
@@ -0,0 +1,1 @@
+INSERT INTO gallery.members (id, email, password) VALUES (1, 'example@example.com', 'abc0709!!');
